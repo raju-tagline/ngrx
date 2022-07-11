41e7ec79470449f11881904926900180deb77e3a
raju-tagline <raju.tagline@gmail.com>
2022-07-11 10:02:58 +0530

    Create an auth guard for the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,33 @@
 // import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {
-    path:'dashboard',
-    loadChildren:() => import('./student-module/student.module').then((m) => m.StudentModule)
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./student-module/student.module').then((m) => m.StudentModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'counter',
     loadChildren: () =>
       import('./counter-module/counter.module').then((m) => m.CounterModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'auth',
-    loadChildren: () =>
-      import('./auth/auth.module').then((m) => m.AuthModule),
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
-    path:'**',
-    redirectTo:'auth',
-  }
+    path: '**',
+    redirectTo: 'auth',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
